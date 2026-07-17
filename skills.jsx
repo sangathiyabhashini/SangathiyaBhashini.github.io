d16a96a5c8a0621b3622f6663d5c4d3cd4273246
sangathiyabhashini <sangathiyabhashini@gmail.com>
2026-07-17 12:59:24 +0530

    Create skills.jsx

---
+++ skills.jsx
@@ -0,0 +1,31 @@
+function Skills() {
+  return (
+    <section>
+
+      <h2>Skills</h2>
+
+      <div className="skills">
+
+        <div>Figma</div>
+
+        <div>React</div>
+
+        <div>HTML</div>
+
+        <div>CSS</div>
+
+        <div>JavaScript</div>
+
+        <div>Python</div>
+
+        <div>Java</div>
+
+        <div>MySQL</div>
+
+      </div>
+
+    </section>
+  );
+}
+
+export default Skills;
