f27aadf3e6bb700df8a27ebf8bca89842099007e
sangathiyabhashini <sangathiyabhashini@gmail.com>
2026-07-17 13:00:29 +0530

    Create contact.jsx

---
+++ contact.jsx
@@ -0,0 +1,39 @@
+function Contact() {
+  return (
+    <section>
+
+      <h2>Contact</h2>
+
+      <p><strong>Name:</strong> Sangathiya Bhashini</p>
+
+      <p>Email: sangathiyabhashini@gmail.com</p>
+
+      <p>
+        LinkedIn:
+        <br />
+        <a
+          href="https://www.linkedin.com/in/sangathiya-bhashini-6a5901296/"
+          target="_blank"
+          rel="noreferrer"
+        >
+          View Profile
+        </a>
+      </p>
+
+      <p>
+        GitHub:
+        <br />
+        <a
+          href="https://github.com/sangathiyabhashini"
+          target="_blank"
+          rel="noreferrer"
+        >
+          View GitHub
+        </a>
+      </p>
+
+    </section>
+  );
+}
+
+export default Contact;
