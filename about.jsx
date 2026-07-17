e30dd64fee799d3bfd787232bafc3105b8c8749c
sangathiyabhashini <sangathiyabhashini@gmail.com>
2026-07-17 12:58:08 +0530

    Add About component with personal information

---
+++ about.jsx
@@ -0,0 +1,16 @@
+function About() {
+  return (
+    <section className="about">
+
+      <h2>About Me</h2>
+
+      <p>
+        I am a Computer Science student passionate about UI/UX Design,
+        Web Development, IoT and Database Management.
+      </p>
+
+    </section>
+  );
+}
+
+export default About;
